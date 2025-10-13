388e1afa8d877fe3545b90defd4af9054cee21c2
Daniel Aviv <dani6laviv@gmail.com>
2025-10-13 16:25:17 +0300

    fixed sha512

--- ports/dnlib/portfile.cmake
+++ ports/dnlib/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO o3wiz/dnlib
     REF v2.0.1
-    SHA512 0
+    SHA512 8983252d027a70463e16f9fe6f0a852936813727d10c621b55cd7f7cd68c16f21c8d95aefb4c48a3b9d7aa4a51b67a1e1aff68d9f1e791378035a9fabd66ff58
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
