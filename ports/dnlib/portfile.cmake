0d9c51848616087db21884bf4a5cbc80ff7d63c0
Daniel Aviv <dani6laviv@gmail.com>
2025-10-13 16:22:08 +0300

    dnlib: v2.0.1 part 1

--- ports/dnlib/portfile.cmake
+++ ports/dnlib/portfile.cmake
@@ -1,14 +1,10 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO o3wiz/dnlib
-    REF v2.0.0
-    SHA512 b906862da78e99b6901dee2e81b3c0012495ff322803d91ee7c2129bbf233b6527195016de228c9ea5a17e2f39d2ce58ab57fee41e3c3a7ebf967e550b3475b3
+    REF v2.0.1
+    SHA512 0
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-)
-
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
