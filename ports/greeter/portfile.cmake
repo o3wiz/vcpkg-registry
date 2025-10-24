d19e471c17f24059eaba890123a056e1aab1b0b0
Daniel Aviv <dani6laviv@gmail.com>
2025-10-24 14:13:51 +0300

    greeter v1.0.1 part 1

--- ports/greeter/portfile.cmake
+++ ports/greeter/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO o3wiz/greeter
-    REF v1.0.0
-    SHA512 af0e18b96b522a1ef3b1208d7bff4ff0c922b2311c65b1777634b458072a2904543d6c2a653e4ebcbfb9b1d46bae2bf57f8d98934e1dcef586bf7148069d5c3e
+    REF v1.0.1
+    SHA512 0
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
