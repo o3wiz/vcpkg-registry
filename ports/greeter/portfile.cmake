a6ba33f27725379d0ad6e30f5e40174fc732d00e
Daniel Aviv <dani6laviv@gmail.com>
2025-10-24 17:23:49 +0300

    greeter v1.0.3 sha512 fixed

--- ports/greeter/portfile.cmake
+++ ports/greeter/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "o3wiz/greeter"
     REF "v${VERSION}"
-    SHA512 0
+    SHA512 "f5acdf72fcabdaaf8ef048aa9d8154d8e8099fba4c590194d37f277f281b6c9b0691d36fa96824417a46a1930f4cea22c210c8e376d1feff41a4f7d5e409a387"
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
