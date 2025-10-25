6477b92140f51b8d5aafa78f29b9939cc60af24e
Daniel Aviv <dani6laviv@gmail.com>
2025-10-25 18:44:24 +0300

    greeter: v1.0.3.(1) sha512 doesn't work yet

--- ports/greeter/portfile.cmake
+++ ports/greeter/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "o3wiz/greeter"
-    REF "v${VERSION}"
-    SHA512 "f5acdf72fcabdaaf8ef048aa9d8154d8e8099fba4c590194d37f277f281b6c9b0691d36fa96824417a46a1930f4cea22c210c8e376d1feff41a4f7d5e409a387"
+    REF "v1.0.3.1"
+    SHA512 0
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
